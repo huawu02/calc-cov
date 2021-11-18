986e3898cd852d62f912056adcfe926298953973
Hua Wu <hua.wu02@gmail.com>
2021-11-17 16:52:14 -0800

    Add calculation of noise correlation matrix

--- calc_cov.m
+++ calc_cov.m
@@ -19,8 +19,8 @@ ne = length(erange);
 nc = pfile.channels;
 
 ksp = zeros(nx, ny, ns, ne, nc);
-img = zeros(nx, ny, ns, ne, nc);
 R   = zeros(nc, nc, ns, ne);
+cor = zeros(nc, nc, ns, ne);
 
 % Reconstruct range of slices and echoes
 %
@@ -31,20 +31,50 @@ for sidx = 1:ns
             % Get raw data
             %
             ksp(:,:,sidx,eidx,c) = GERecon('Pfile.KSpace', srange(sidx), erange(eidx), c, pfile);
-            img(:,:,sidx,eidx,c) = GERecon('Transform', squeeze(ksp(:,:,sidx,eidx,c)));
         end
-        R(:,:,sidx,eidx) = cov(reshape(img(:,:,sidx,eidx,:), [nx*ny,nc]));   
+        R(:,:,sidx,eidx) = cov(reshape(ksp(:,:,sidx,eidx,:), [nx*ny,nc]));   
+        k_std = std(reshape(ksp(:,:,sidx,eidx,:), [], nc));
+        for i = 1:nc
+            for j = 1:nc
+                cor(i,j,sidx,eidx) = R(i,j,sidx,eidx)/k_std(i)/k_std(j);
+            end
+        end
+        R(:,:,sidx,eidx) = R(:,:,sidx,eidx) / sum(diag(abs(R(:,:,sidx,eidx)))) * nc;  % normalize to an average of 1 in each channel
+        tmp = cor(:,:,sidx,eidx);
+        meancor(sidx,eidx) = mean(abs(tmp(find(triu(tmp,1)))));
     end
 end
 
-f = figure('visible','on');
+f1 = figure('visible','on');
 if ns*ne > 1
     montage(abs(reshape(R, [nc,nc,ns*ne]))); % montage will somehow change the image resolution automatically
 else
-    imagesc(abs(reshape(R, [nc,nc,ns*ne])));
+    imagesc(abs(R));
 end
+title('Noise covariance normalized to the number of channels');
 caxis('auto');colormap(jet(256));colorbar;
-saveas(f,fullfile(outpath, 'Noise_covariance.png'));
+saveas(f1,fullfile(outpath, 'Noise_covariance.png'));
 save(fullfile(outpath, 'Noise_covariance.mat'), 'R');
-%close(f);
+
+f2 = figure('visible','on');
+if ns*ne > 1
+    montage(abs(reshape(cor, [nc,nc,ns*ne]))); % montage will somehow change the image resolution automatically
+else
+    imagesc(abs(cor)); 
+end
+title(sprintf('Noise correlation, mean correlation %.4f', mean(meancor,'all')));
+caxis('auto');colormap(jet(256));colorbar;
+saveas(f2,fullfile(outpath, 'Noise_correlation.png'));
+save(fullfile(outpath, 'Noise_correlation.mat'), 'cor');
+
+f3 = figure('visible','on');
+for sidx = 1:ns
+    for eidx = 1:ne
+        plot(sort(abs(diag(R(:,:,sidx,eidx))),'descend'));
+    end
+end
+title('Noise covariance matrix diagonal elements in descending order');
+saveas(f3,fullfile(outpath, 'Noise_covariance_diagonal.png'));
+
+% close all;
 
